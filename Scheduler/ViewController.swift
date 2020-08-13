2fcf4862731e15177bc068998d5e14934f232ed6
Elbert John Alejo Orozco <elbertjohn.orozco@mynt.xyz>
2020-08-13 08:14:21 +0800

    Implement base xcode project

---
+++ Scheduler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scheduler
+//
+//  Created by Elbert John Orozco on 2020/08/13.
+//  Copyright © 2020 mynt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
