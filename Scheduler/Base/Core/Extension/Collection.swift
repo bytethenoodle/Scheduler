406e512726d7100f03e114f3c81654d9780dc62d
Elbert John Alejo Orozco <elbertjohn.orozco@mynt.xyz>
2020-08-28 18:05:59 +0800

    Implement collection extension for safe subscript

---
+++ Scheduler/Base/Core/Extension/Collection.swift
@@ -0,0 +1,15 @@
+//
+//  Collection.swift
+//  Scheduler
+//
+//  Created by Elbert John Orozco on 2020/08/28.
+//  Copyright © 2020 mynt. All rights reserved.
+//
+
+import Foundation
+
+extension Collection {
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
